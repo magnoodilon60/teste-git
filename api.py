5d38ed9620467c6c380c16e723803ffc14dca8ce
magnoodilon60 <magnoodilon60@gmail.com>
2021-03-19 13:25:17 -0300

    segunda rora

--- api.py
+++ api.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def ok(nome):
     return "Nome: "+nome
 
+@app.route('/segunda/')
+def page2():
+    return '<h1> Esse foi um teste de api flask. </h1>'
+
 app.run(host="0.0.0.0", port= 2000, debug= False)
 
 #teste feito seguindo o tutorial no youtube no canal do erikson bastos, obg por compartilhar o conhecimento
