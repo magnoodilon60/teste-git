4aee9d59322b35eb50576e1628b5e6e2ab604afc
magnoodilon60 <magnoodilon60@gmail.com>
2021-03-19 09:41:59 -0300

    ajutado a lógica para o game over, arquivo avinhacao.py

--- advinhacao.py
+++ advinhacao.py
@@ -4,7 +4,7 @@ print(50 * '#')
 
 numero_secreto = 42
 total_tentativas = 3
-rodada = 1
+#rodada = 1
 
 
 for rodada in range(1, total_tentativas + 1):
@@ -16,16 +16,20 @@ for rodada in range(1, total_tentativas + 1):
     acertou = chute == numero_secreto
     maior   = chute > numero_secreto
     menor   = chute < numero_secreto
-    game_over = (rodada ==3 and chute > numero_secreto and chute < numero_secreto)
+    #rodada_t = rodada 
+    game_over = (rodada ==3 and chute > numero_secreto or chute < numero_secreto)
     if(acertou):
         print("Parabéns!!! Você acertou!!!!")
         break
-    elif(maior):
-        print('Você erro!!! o número que voçê informou e maior que o némero secreto')
-    elif(menor):
-        print('Você erro!!! o número que voçê informou e menor que o némero secreto')
-    elif(game_over):
+    else:
+        if(maior):
+            print('Você erro!!! o número que voçê informou e maior que o némero secreto')
+        elif(menor):
+            print('Você erro!!! o número que voçê informou e menor que o némero secreto')
+    
+    if(game_over):
         print('GAME OVER')
+    
 
 print('Fim do jogo')
 
