f115ba796366751c7fb0ca289cc12b2ae6f1fced
magnoodilon60 <magnoodilon60@gmail.com>
2021-03-21 00:54:46 -0300

    teste cod bar

---
+++ gerador_barcode.py
@@ -0,0 +1,12 @@
+# requeriments
+#deve instalar o python-barcode
+#pip install python-barcode
+
+from barcode import Code128
+from barcode.writer import ImageWriter
+
+#Gera o código de barras e salvar no caminho desejado
+with open(r'test1.jpeg', 'wb') as f:
+    Code128('210321000101', writer=ImageWriter()).write(f)
+
+
