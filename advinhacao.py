f6be2967f4f7a4e9f43b081c2552b9fdb40c4bfb
magnoodilon60 <magnoodilon60@gmail.com>
2021-03-20 23:25:03 -0300

    deu rim

--- advinhacao.py
+++ advinhacao.py
@@ -1,17 +1,19 @@
+import random
+
 print(50 * '#')
 print("Vamos jogar a adivinhação?")
 print(50 * '#')
 
-numero_secreto = 42
+numero_secreto = random.randrange(1, 101)
 total_tentativas = 3
 #rodada = 1
 
 
 for rodada in range(1, total_tentativas + 1):
     print(f'Tentativa {rodada} de {total_tentativas}')
-    chute = int(input("Digite o seu número: "))
+    chute = int(input("Digite o seu número entre 1 e 100: "))
     print(f'Você digitou o número: {chute}')
-
+    
 
     acertou = chute == numero_secreto
     maior   = chute > numero_secreto
@@ -27,9 +29,10 @@ for rodada in range(1, total_tentativas + 1):
         elif(menor):
             print('Você erro!!! o número que voçê informou e menor que o némero secreto')
     
-    if(game_over):
-        print('GAME OVER')
-    
+        elif(game_over):
+            print('GAME OVER')
+            print(f"O número secreto foi {numero_secreto}")    
 
 print('Fim do jogo')
+
 
